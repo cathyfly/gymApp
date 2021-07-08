e0f0c7f8f33dc97dc3973120cc46a64e432ad2b5
“[firstname <cathyflynnmedia@gmail.com>
2021-07-08 15:14:45 +0100

    static pages created

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,10 @@
+class StaticPageController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  #get 'static_page/home'
+  #get 'static_page/contact'
+  get 'contact', to: 'static_page#contact'
+
+  #get 'static_page/about
+  get 'about', to: 'static_page#about'
+  root to:'static_page#home' #root
   resources :activities
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
